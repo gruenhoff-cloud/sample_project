e1c044bd3fa99084cfd1f4fa60fc9af116a0cd09
gruenhoff-cloud <sebastiangruenhoff@gmail.com>
2025-10-06 00:33:51 +0200

    added prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $13', 'Pasta - $23.99', 'Burger - $22.99', 'Salmon - $16'];
+const dessertMenu = ['Cake - $14', 'Ice Cream - $24.99', 'Pudding - $23.99', 'Fruit Salad - $17'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
