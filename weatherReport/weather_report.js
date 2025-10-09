40bdb5aa1b0ac45338fe6cb18e511ef14e9b5bcc
gruenhoff-cloud <sebastiangruenhoff@gmail.com>
2025-10-09 16:54:27 +0200

    added structure and fetch api per name

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,22 @@
+function showWeatherDetails(event) {
+  event.preventDefault();
+  const city = document.getElementById('city').value;
+  const apiKey = 'c66050100176ff98404151897aee2223'
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                               <p>Temperature: ${data.main.temp} &#8451;</p>
+                               <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+      console.error('Error fetching weather:', error);
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<p>Failend to fetch weather. Please try again.</p>`;
+    });
+}
+
+document.getElementById('weatherForm').addEventListener('submit', showWeatherDetails);
