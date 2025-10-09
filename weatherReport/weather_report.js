297c382be09fe3752916e705cbdc25e2d91adf6e
gruenhoff-cloud <sebastiangruenhoff@gmail.com>
2025-10-09 17:13:36 +0200

    added fetching from lat and lon

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,6 +1,6 @@
 function showWeatherDetails(event) {
   event.preventDefault();
-  const city = document.getElementById('city').value;
+  const lat = document.getElementById('lat').value;
   const apiKey = 'c66050100176ff98404151897aee2223'
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
@@ -19,4 +19,28 @@ function showWeatherDetails(event) {
     });
 }
 
+function showWeatherDetails1(event) {
+  event.preventDefault();
+  const lat = document.getElementById('lat').value;
+  const lon = document.getElementById('lon').value;
+  const apiKey = 'c66050100176ff98404151897aee2223'
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                               <p>Temperature: ${data.main.temp} &#8451;</p>
+                               <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+      console.error('Error fetching weather:', error);
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<p>Failend to fetch weather. Please try again.</p>`;
+    });
+}
+
+
 document.getElementById('weatherForm').addEventListener('submit', showWeatherDetails);
+document.getElementById('weatherForm1').addEventListener('submit', showWeatherDetails1);
