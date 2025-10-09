a4e78e5f11161eb0db7149dc735b4e1ae58ff844
gruenhoff-cloud <sebastiangruenhoff@gmail.com>
2025-10-09 15:26:21 +0200

    added own fetch to url

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -1,49 +1,22 @@
-var xhr = new XMLHttpRequest();
-var url = './health.json';
+let xhr = new XMLHttpRequest();
+let url = 'https://raw.githubusercontent.com/cjbatin/Swift4-Decoding-JSON-Using-Codable/master/WeatherForecast/StubData/londonWeather.json';
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
+xhr.onload = () => {
+  var data = xhr.response;
+  var dataDiv = document.getElementById('city');
 
-xhr.onload = function() {
-  var articles = xhr.response.articles;
-  var articlesDiv = document.getElementById('articles');
-  articles.forEach((article) => {
-    var articleDiv = document.createElement('div');
-    articleDiv.classList.add('article');
+  var name = document.createElement('h2');
+  name.textContent = data.name;
+  
+  var weatherDiv = document.createElement('h3');
+  weatherDiv.textContent = `Weather: ${data.weather[0].main}`;
 
-    var title = document.createElement('h2');
-    title.textContent = article.title;
+  var windDiv = document.createElement('h3');
+  windDiv.textContent = `Wind Speed: ${data.wind.speed}`;
 
-    var description = document.createElement('p');
-    description.textContent = article.description;
-
-    var waysHeader = document.createElement('h3');
-    waysHeader.textContent = 'Ways to Achieve:';
-
-    var waysList = document.createElement('ul');
-    article.ways_to_achieve.forEach((way) => {
-      var listItem = document.createElement('li');
-      listItem.textContent = way;
-      waysList.appendChild(listItem);
-    });
-
-    var benefitsHeader = document.createElement('h3');
-    benefitsHeader.textContent = 'Benefits:';
-
-    var benefitsList = document.createElement('ul');
-    article.benefits.forEach((benefit) => {
-      var listItem = document.createElement('li');
-      listItem.textContent = benefit;
-      benefitsList.appendChild(listItem);
-    });
-
-    articleDiv.appendChild(title);
-    articleDiv.appendChild(description);
-    articleDiv.appendChild(waysHeader);
-    articleDiv.appendChild(waysList);
-    articleDiv.appendChild(benefitsHeader);
-    articleDiv.appendChild(benefitsList);
-
-    articlesDiv.appendChild(articleDiv);
-  });
-}
+  dataDiv.appendChild(name);
+  dataDiv.appendChild(weatherDiv);
+  dataDiv.appendChild(windDiv);
+};
 xhr.send();
